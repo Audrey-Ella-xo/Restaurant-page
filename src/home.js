b676e1b0fdd04e1a4587cbf1e3debecd3f4fd63d
Audrey-Ella-xo <anitaudrey@gmail.com>
2020-07-27 03:47:30 +0100

    Add logic and functions to Menu, Contact and Home pages

--- src/home.js
+++ src/home.js
@@ -3,21 +3,35 @@ const homePage = () => {
   contentElement.id = 'homeContent';
   contentElement.classList.add('content', 'container');
 
+  const cardFactory = (heading, body, small) => ({ heading, body, small });
+
+  const cardObject = {
+    cardOne: cardFactory('Discount', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque ex corrupti earum! Debitis dolorem quis provident temporibus at, voluptatibus voluptates corrupti rem corporis illo molestias quae quibusdam eos autem cum adipisci.', 'learn More'),
+
+    cardTwo: cardFactory('About', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque ex corrupti earum! Debitis dolorem quis provident temporibus at, voluptatibus voluptates corrupti rem corporis illo molestias quae quibusdam eos autem cum adipisci.', 'learn More'),
+
+    cardThree: cardFactory('Meet Staff', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque ex corrupti earum! Debitis dolorem quis provident temporibus at, voluptatibus voluptates corrupti rem corporis illo molestias quae quibusdam eos autem cum adipisci.', 'learn More'),
+  };
+
   const cardPannel = document.createElement('div');
   cardPannel.classList.add('add-panel');
 
-  const card1 = document.createElement('div');
-  card1.classList.add('card');
-  const h2 = document.createElement('h2');
-  h2.innerHTML = 'Discount';
-  card1.appendChild(h2);
-  const p = document.createElement('p');
-  p.innerHTML = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque ex corrupti earum! Debitis dolorem quis provident temporibus at, voluptatibus voluptates corrupti rem corporis illo molestias quae quibusdam eos autem cum adipisci.';
-  card1.appendChild(p);
-  const small = document.createElement('small');
-  small.innerHTML = 'Learn More';
-  card1.appendChild(small);
-  cardPannel.appendChild(card1);
+  Object.keys(cardObject).forEach(key => {
+    const card1 = document.createElement('div');
+    card1.classList.add('card');
+    const h2 = document.createElement('h2');
+    const p = document.createElement('p');
+    const small = document.createElement('small');
+
+    h2.innerHTML = cardObject[key].heading;
+    p.innerHTML = cardObject[key].body;
+    small.innerHTML = cardObject[key].small;
+
+    card1.appendChild(h2);
+    card1.appendChild(p);
+    card1.appendChild(small);
+    cardPannel.appendChild(card1);
+  });
 
   contentElement.appendChild(cardPannel);
 
--- src/menu.js
+++ src/menu.js
@@ -2,10 +2,52 @@ const menuPage = () => {
   const contentElement = document.createElement('div');
   contentElement.id = 'menuContent';
   contentElement.classList.add('content', 'container', 'menu-list');
-  const heading = document.createElement('h1');
+  const heading = document.createElement('h2');
   heading.innerHTML = 'THE DELICIOUS DISHES';
 
+  const starter = document.createElement('div');
+  starter.innerHTML = `
+    <h3>Starter</h3>
+    <ul>
+        <li>Lorem ipsum dolor sit amet.............. #50</li>
+        <li>Lorem ipsum dolor sit amet.............. #50</li>
+        <li>Lorem ipsum dolor sit amet.............. #50</li>
+        <li>Lorem ipsum dolor sit amet.............. #50</li>
+    </ul>
+  `;
+
+  const main = document.createElement('div');
+  main.innerHTML = `
+  <h3>Main course</h3>
+  <ul>
+      <li>Lorem ipsum dolor sit amet.............. #50</li>
+      <li>Lorem ipsum dolor sit amet.............. #50</li>
+      <li>Lorem ipsum dolor sit amet.............. #50</li>
+      <li>Lorem ipsum dolor sit amet.............. #50</li>
+  </ul>
+
+  <ul>
+      <li>Lorem ipsum dolor sit amet.............. #50</li>
+      <li>Lorem ipsum dolor sit amet.............. #50</li>
+      <li>Lorem ipsum dolor sit amet.............. #50</li>
+      <li>Lorem ipsum dolor sit amet.............. #50</li>
+  </ul>
+  `;
+
+  const dessert = document.createElement('div');
+  dessert.innerHTML = `
+  <h3>Desert</h3>
+                <ul>
+                    <li>Lorem ipsum dolor sit amet.............. #50</li>
+                    <li>Lorem ipsum dolor sit amet.............. #50</li>
+                    <li>Lorem ipsum dolor sit amet.............. #50</li>
+                    <li>Lorem ipsum dolor sit amet.............. #50</li>
+                </ul>
+  `;
   contentElement.appendChild(heading);
+  contentElement.appendChild(starter);
+  contentElement.appendChild(main);
+  contentElement.appendChild(dessert);
 
   document.body.appendChild(contentElement);
 };
